de93657e0a399255fd53c0add98473785011d407
Marcson <mss12@cin.ufpe>
2021-07-19 22:26:40 -0300

    adição das chaves P e F e correção do modelo EER

--- 2º_entrega/banco_de_Dados_Reserva_Hospedagem.sql
+++ 2º_entrega/banco_de_Dados_Reserva_Hospedagem.sql
@@ -16,16 +16,19 @@ constraint funcionario_pk primary key (codigo_funcionario)
 
 );
 
+drop table hospedagem;
+
 create table hospedagem(
 status_hospedagem char(45),
 forma_pagamento varchar(45),
-constraint status_hospedagem_pk primary key (status_hospedagem)
-
+codigio_funcionario char(10),
+constraint status_hospedagem_pk primary key (status_hospedagem),
+constraint codigio_funcionario_fk foreign key (codigio_funcionario) references funcionario (codigio_funcionario)
 );
 
 
 /*
-
+adicionar
 create table hospede_has_hospedagem(
 hospede_codigo_hospede varchar(45),
 hospedagem_status_hospedagem varchar(45),
@@ -39,16 +42,20 @@ codigo_hospede char(45),
 nome varchar(45),
 motivo_hospedagem varchar(45),
 numero_contato char(45),
-constraint hospede primary key (codigo_hospede)
+constraint hospede primary key (codigo_hospede),
+constraint codigio_funcionario_fk foreign key (codigio_funcionario) references funcionario (codigio_funcionario)
 );
 
-
+drop table quarto;
 create table quarto(
 status_quarto varchar(45),
 numero_quarto char(3),
 acomodacao varchar(10),
-constraint status_quarto_pk primary key (status_quarto)
+codigo_reserva char(10),
+constraint numero_quarto_pk primary key (status_quarto),
+constraint codigo_reserva_fk foreign key (codigo_reserva) references hospede (codigo_reserva)
 );
+
 drop table reserva;
 create table reserva(
 codigo_reserva char(10),
@@ -56,14 +63,19 @@ data_reserva varchar(10),
 numero_quarto char(3),
 tipo_quarto varchar(10),
 codigo_hospede varchar(10),
+codigo_funcionario varchar(10),
 constraint reserva_pk primary key (codigo_reserva),
-constraint codigo_hospede_fk foreign key (codigo_hospede) references hospede (codigo_hospede)
+constraint codigo_hospede_fk foreign key (codigo_hospede) references hospede (codigo_hospede),
+constraint codigo_funcionario_fk foreign key (codigo_funcionario) references funcionario (codigo_funcionario)
 );
 
+drop table tipo_quarto;
 create table tipo_quarto(
 codigo_tipo_quarto char(10),
 acomodacao varchar(10),
-constraint tipo_quarto_pk primary key (codigo_tipo_quarto)
+numero_quarto char(3),
+constraint tipo_quarto_pk primary key (codigo_tipo_quarto),
+constraint numero_quarto_fk foreign key (numero_quarto) references quarto (numero_quarto)
 );
 
 
