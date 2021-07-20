7a7fe0150a2a0e9b3ccef59c8040da15127144c8
Marcson <mss12@cin.ufpe>
2021-07-19 23:31:45 -0300

    adição de iserts

---
+++ 2º_entrega/Povoamento_ER_Entrega2_SysHotel.sql
@@ -0,0 +1,94 @@
+
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (103,'Gustavo Henrique Amadio');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (223,'Lucas Vitoriano da Silva');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (333,'Gizelle Guanabara Gomez');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (433,'José Ronaldo da Silva');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (501,'Graça Araujo de Lima');
+
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (342,'Tales Henrique Amadio');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (486,'Mario Vitoriano da Silva');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (435,'Marcia Guanabara Gomez');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (109,'José Carlos da Silva');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (657,'Graça Aranha de Souza');
+
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (853,'Gustavo Henrique Amadio');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (856,'Lucas Carlos Sá da Silva');
+INSERT INTO FUNCIONARIO (codigo_funcionario, nome) VALUES (546,'Gabriela Atunes Gomez');
+
+
+
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (1,'Felipe Garcia','Viagem de férias','+558198111-1098');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (2,'Lucas Velozo','Viagem de trabalho','+558198334-0990');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (3,'Ingrid Victória','Dia especial com esposa','+558198139-7678');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (4,'Vitoriano Henrique','Cheguei recentemente no estado e queria me abrigar','+558198113-5880');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (5,'Ygor Libriano','Visitando a região','+558199778-7788');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (6,'Iranir Fonseca','Viagem por conta do trabalho','+558195678-9685');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (7,'João Aluizio','Queria ver como um hotel funciona por dentro','+558199888-7784');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (8,'Nura Vitória','Não tinha onde passar a noite','+558196685-1124');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (9,'Amélia Santos','Não tenho tantos motivos','+558191168-5788');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (10,'Lucas Henrique','Viagem de mudança','+558199009-3357');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (11,'Receba Amants','Férias','+558191130-8906');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (12,'Bruna Oliveira','Região bonita para passar uns dias','+558193339-1345');
+INSERT INTO HOSPEDE (codigo_hospede, nome, motivo_hospedagem, numero_contato) VALUES (13,'Diego Santiago','Não sei','+558192258-9907');
+
+
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (1111,'13/08/2021','01', 3);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (2222,'29/08/2021','04', 3);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (3333,'29/08/2021','13', 3);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (4444,'16/08/2021','20', 1);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (5555,'21/08/2021','02', 2);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (6666,'27/08/2021','05', 4);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (7777,'13/08/2021','06', 1);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (8888,'19/08/2021','08', 3);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (9999,'14/08/2021','09', 2);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (1010,'16/08/2021','11', 1);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (1212,'16/08/2021','15', 4);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (1313,'22/08/2021','16', 2);
+INSERT INTO RESERVA (codigo_reserva, data_reserva, numero_quarto, tipo_quarto) VALUES (1414,'25/08/2021','18', 3);
+
+
+INSERT INTO TIPO_QUARTO (codigo_tipo_quarto, acomodacao) VALUES (1,'1 Quarto');
+INSERT INTO TIPO_QUARTO (codigo_tipo_quarto, acomodacao) VALUES (2,'2 Quartos');
+INSERT INTO TIPO_QUARTO (codigo_tipo_quarto, acomodacao) VALUES (3,'2 Quatos sendo 1 suíte'); 
+INSERT INTO TIPO_QUARTO (codigo_tipo_quarto, acomodacao) VALUES (4,'2 Quartos com 1 banheira de hidromassagem');
+
+/* corrigir essa parte  do quarto*/
+
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (1, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (2, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (3, 'vago');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (4, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (5, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (6, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (7, 'vago');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (8, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (9, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (10, 'vago');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (11, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (12, 'vago');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (13, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (14, 'vago');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (15, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (16, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (17, 'vago');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (18, 'preenchido');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (19, 'vago');
+INSERT INTO QUARTO (numero_quarto, status_quarto) VALUES (20, 'preenchido');
+
+/* corrigir essa parte  da hospedagem */
+
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','debito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('negada', NULL);
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','credito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','debito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','credito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','debito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','debito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','debito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','credito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','credito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','credito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','credito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('validada','credito');
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('negada', NULL);
+INSERT INTO HOSPEDAGEM (status_hospedagem, forma_pagamento) VALUES ('negada', NULL);
--- 2º_entrega/banco_de_Dados_Reserva_Hospedagem.sql
+++ 2º_entrega/banco_de_Dados_Reserva_Hospedagem.sql
@@ -10,7 +10,7 @@ drop table tipo_quarto;
 use reservas_hospedagem;
 
 create table funcionario(
-codigo_funcionario char(10),
+codigo_funcionario char(45),
 nome varchar(45),
 constraint funcionario_pk primary key (codigo_funcionario)
 
@@ -20,7 +20,7 @@ constraint funcionario_pk primary key (codigo_funcionario)
 create table hospedagem(
 status_hospedagem char(45),
 forma_pagamento varchar(45),
-codigio_funcionario char(10),
+codigio_funcionario char(45),
 constraint status_hospedagem_pk primary key (status_hospedagem),
 constraint codigio_funcionario_fk foreign key (codigio_funcionario) references funcionario (codigio_funcionario)
 );
@@ -41,24 +41,23 @@ constraint hospede primary key (codigo_hospede),
 constraint codigio_funcionario_fk foreign key (codigio_funcionario) references funcionario (codigio_funcionario)
 );
 
-
+drop table quarto;
 create table quarto(
+numero_quarto varchar(45),
 status_quarto varchar(45),
-numero_quarto char(3),
-acomodacao varchar(10),
-codigo_reserva char(10),
+codigo_reserva char(45),
 constraint numero_quarto_pk primary key (status_quarto),
 constraint codigo_reserva_fk foreign key (codigo_reserva) references hospede (codigo_reserva)
 );
 
 
 create table reserva(
-codigo_reserva char(10),
-data_reserva varchar(10),
-numero_quarto char(3),
-tipo_quarto varchar(10),
-codigo_hospede varchar(10),
-codigo_funcionario varchar(10),
+codigo_reserva char(45),
+data_reserva varchar(45),
+numero_quarto char(45),
+tipo_quarto varchar(45),
+codigo_hospede varchar(45),
+codigo_funcionario varchar(45),
 constraint reserva_pk primary key (codigo_reserva),
 constraint codigo_hospede_fk foreign key (codigo_hospede) references hospede (codigo_hospede),
 constraint codigo_funcionario_fk foreign key (codigo_funcionario) references funcionario (codigo_funcionario)
@@ -66,9 +65,9 @@ constraint codigo_funcionario_fk foreign key (codigo_funcionario) references fun
 
 
 create table tipo_quarto(
-codigo_tipo_quarto char(10),
-acomodacao varchar(10),
-numero_quarto char(3),
+codigo_tipo_quarto char(45),
+acomodacao varchar(45),
+numero_quarto char(45),
 constraint tipo_quarto_pk primary key (codigo_tipo_quarto),
 constraint numero_quarto_fk foreign key (numero_quarto) references quarto (numero_quarto)
 );
